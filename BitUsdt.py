a23907c4637dae88894b404a940ea352cc26a23d
naeimnb <naeim1987@gmail.com>
2021-03-22 16:05:02 +0430

    remove $ at price remove USD at price and split high and low  price and split open and close  price at bit usd

--- BitUsdt.py
+++ BitUsdt.py
@@ -31,13 +31,47 @@ for item in myBitSoup:
     select+=1
 
 
+bit_live=bit_live[1:].split(' ')
+bit_live=bit_live[0]
 print('bit_live: ',bit_live)
+
+bit_24h_Volume=bit_24h_Volume[1:].split(' ')
+bit_24h_Volume=bit_24h_Volume[0]
 print('bit_24h_volume: ',bit_24h_Volume)
+
+bit_Circulating_Supply=bit_Circulating_Supply.split(' ')
+bit_Circulating_Supply=bit_Circulating_Supply[0]
 print('bit_Circulating_Supply: ',bit_Circulating_Supply)
+
+bit_Market_Cap=bit_Market_Cap[1:].split(' ')
+bit_Market_Cap=bit_Market_Cap[0]
 print('bit_Market_Cap: ',bit_Market_Cap)
+
+bit_Max_Supply=bit_Max_Supply.split(' ')
+bit_Max_Supply=bit_Max_Supply[0]
 print('bit_Max_Supply: ',bit_Max_Supply)
-print('bit_Yesterday_High_Low: ',bit_Yesterday_High_Low)
+
+bit_Yesterday_High_Low=bit_Yesterday_High_Low[1:].split(' ')
+bit_Yesterday_High=bit_Yesterday_High_Low[0]
+print('bit_Yesterday_High: ',bit_Yesterday_High)
+
+bit_Yesterday_Low=bit_Yesterday_High_Low[3]
+bit_Yesterday_Low=bit_Yesterday_Low[1:]
+print('bit_YesterdayLow: ',bit_Yesterday_Low)
+
+bit_Yesterday_Market_Cap=bit_Yesterday_Market_Cap[1:].split(' ')
+bit_Yesterday_Market_Cap=bit_Yesterday_Market_Cap[0]
 print('bit_Yesterday_Market_Cap: ',bit_Yesterday_Market_Cap)
-print('bit_Yesterday_Open_Close: ',bit_Yesterday_Open_Close)
+
+bit_Yesterday_Open_Close=bit_Yesterday_Open_Close[1:].split(' ')
+bit_Yesterday_Open=bit_Yesterday_Open_Close[0]
+print('bit_Yesterday_Open: ',bit_Yesterday_Open)
+
+bit_Yesterday_Close=bit_Yesterday_Open_Close[3]
+bit_Yesterday_Close=bit_Yesterday_Close[1:]
+print('bit_Yesterday_Close: ',bit_Yesterday_Close)
+
+bit_Yesterday_Volume=bit_Yesterday_Volume[1:].split(' ')
+bit_Yesterday_Volume=bit_Yesterday_Volume[0]
 print('bit_Yesterday_Volume: ',bit_Yesterday_Volume)
 
